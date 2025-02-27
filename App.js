a0bb2a45902294fa2d45c794d7f78252a5718217
SDD3108 <mrbimson1@gmail.com>
2025-02-28 02:57:34 +0500

    push testing

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+// test
 import React,{useState} from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
